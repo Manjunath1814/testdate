5950da7b3caabcd41accb0485480c7860a68f32d
Manjunath1814 <manuvlogs982@gmail.com>
2026-06-28 19:58:18 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  // Paste your Firebase config here
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBK8VWsNWnDFEHQa-tSY7rFxQz6zbIKVEo",
+  authDomain: "testdate-64ee3.firebaseapp.com",
+  projectId: "testdate-64ee3",
+  storageBucket: "testdate-64ee3.firebasestorage.app",
+  messagingSenderId: "212768048889",
+  appId: "1:212768048889:web:468dc2026122b7b172c428",
+  measurementId: "G-H7B0670KW0"
+};
